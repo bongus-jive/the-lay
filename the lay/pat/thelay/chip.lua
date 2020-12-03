67fcb5af54ab6b08cbf41483f79aa5734bf2f2a5
patmanf <27874300+patmanf@users.noreply.github.com>
2020-12-03 17:53:00 -0600

    the lay

---
+++ the lay/pat/thelay/chip.lua
@@ -0,0 +1,16 @@
+require "/scripts/util.lua"
+
+function init()
+	activeItem.setTwoHandedGrip(false)
+	
+	ratio = 0.5
+	speed = config.getParameter("speed", 1)
+end
+
+function update(dt)
+	ratio = util.wrap(ratio + dt * speed, -1, 1)
+
+	animator.resetTransformationGroup("chip")
+	animator.scaleTransformationGroup("chip", 0.025)
+	animator.translateTransformationGroup("chip", {0, util.easeInOutSin(math.abs(ratio), -0.25, 0.25)})
+end
