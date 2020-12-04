3175e6c27c245278a444647b45bb128c0b647b9e
patmanf <27874300+patmanf@users.noreply.github.com>
2020-12-03 22:43:00 -0600

    reduce max stack to 100

--- the lay/pat/thelay/chip.lua
+++ the lay/pat/thelay/chip.lua
@@ -14,7 +14,7 @@ function update(dt)
 	animator.scaleTransformationGroup("chip", 0.025)
 	animator.translateTransformationGroup("chip", {0, util.easeInOutSin(math.abs(ratio), -0.25, 0.25)})
 	
-	for i = 2, 1000 do
+	for i = 2, 100 do
 		animator.setPartTag("chip"..i, "vis", (i <= item.count() and "" or "?multiply=0000"))
 	end
 end
