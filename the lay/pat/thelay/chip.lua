3f9a5b5ce0ed5a4eb5d143a16aaf4285df168ca3
patmanf <27874300+patmanf@users.noreply.github.com>
2020-12-12 15:56:00 -0600

    configurable chip floating

--- the lay/pat/thelay/chip.lua
+++ the lay/pat/thelay/chip.lua
@@ -5,16 +5,25 @@ function init()
 	
 	ratio = 0.5
 	speed = config.getParameter("speed", 1)
+	scale = config.getParameter("scale", 0.025)
+	hover = config.getParameter("hover", {-0.25, 0.25})
+	stack = config.getParameter("stack", true)
+	stackDone = false
 end
 
 function update(dt)
 	ratio = util.wrap(ratio + dt * speed, -1, 1)
 
 	animator.resetTransformationGroup("chip")
-	animator.scaleTransformationGroup("chip", 0.025)
-	animator.translateTransformationGroup("chip", {0, util.easeInOutSin(math.abs(ratio), -0.25, 0.25)})
+	animator.scaleTransformationGroup("chip", scale)
+	animator.translateTransformationGroup("chip", {0, util.easeInOutSin(math.abs(ratio), hover[1], hover[2])})
 	
-	for i = 2, 100 do
-		animator.setPartTag("chip"..i, "vis", (i <= item.count() and "" or "?multiply=0000"))
+	if not stackDone then
+		for i = 2, 100 do
+			animator.setPartTag("chip"..i, "vis", (i <= item.count() and "" or "?multiply=0000"))
+		end
+		if stack then
+			stackDone = true
+		end
 	end
 end
